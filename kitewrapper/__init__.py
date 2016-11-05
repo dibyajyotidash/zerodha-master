e01e0f9f4a17cdfcfa5260751c6443f639802356
Swapnil Jariwala <swapniljariwala@gmail.com>
2016-11-05 23:55:14 +0530

    tested order_place, modify, cancel

--- kitewrapper/__init__.py
+++ kitewrapper/__init__.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup
 import requests
 from kiteconnect import KiteConnect
 import kiteconnect.exceptions as ex
+
 USER_AGENT_STR = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
-BASE_REFERER  = 'https://kite.zerodha.com/'
+BASE_REFERER = 'https://kite.zerodha.com/'
 LOGIN_SUBMIT_URL = 'https://kite.zerodha.com/'
 
 
@@ -17,54 +18,54 @@ class KiteFront(KiteConnect):
         "api.validate": "/session/token",
         "api.invalidate": "/session/token",
         "user.margins": "/margins/{segment}",
-        
-        "session" : "/session", #checked
 
-        "orders": "/orders", #checked
-        "trades": "/trades", #checked
-        "orders.info": "/orders/{order_id}", #checked
+        "session": "/session",  # checked
 
-        "orders.place": "/orders", #checked
-        "orders.modify": "/orders/{variety}/{order_id}", 
-        "orders.cancel": "/orders/{variety}/{order_id}",
+        "orders": "/orders",  # checked
+        "trades": "/trades",  # checked
+        "orders.info": "/orders/{order_id}",  # checked
+
+        "orders.place": "/orders",  # checked
+        "orders.modify": "/orders/{order_id}",
+        "orders.cancel": "/orders/{order_id}",
         "orders.trades": "/orders/{order_id}/trades",
 
-        "portfolio.positions": "/positions", #checked
-        "portfolio.holdings": "/holdings", #checked
+        "portfolio.positions": "/positions",  # checked
+        "portfolio.holdings": "/holdings",  # checked
         "portfolio.positions.modify": "/positions",
 
-        "market.instruments.all": "/instruments",
-        "market.instruments": "/instruments/{exchange}",
-        "market.quote": "/instruments/{exchange}/{tradingsymbol}",
+        "market.instruments.all": "https://api.kite.trade/instruments",
+        "market.instruments": "https://api.kite.trade/instruments/{exchange}",
+        "market.quote": "https://api.kite.trade/instruments/{exchange}/{tradingsymbol}",
         "market.historical": "/instruments/historical/{instrument_token}/{interval}",
         "market.trigger_range": "/instruments/{exchange}/{tradingsymbol}/trigger_range"
-        }  
-    
+    }
+
     def __init__(self, auth, *args, **kwargs):
         if not kwargs.get('api_key'):
             kwargs['api_key'] = 'kitefront'
         self._auth = auth
-    
+
         self.session = requests.Session()
         self.headers_normal = {
-                                'user-agent': USER_AGENT_STR,
-                                'Host': 'kite.zerodha.com',
-                                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                                'Accept-Encoding': 'gzip, deflate, sdch',
-                                'Referer': BASE_REFERER
-                                }
+            'user-agent': USER_AGENT_STR,
+            'Host': 'kite.zerodha.com',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate, sdch',
+            'Referer': BASE_REFERER
+        }
         self.headers_json = {
-                                'user-agent': USER_AGENT_STR,
-                                'Host': 'kite.zerodha.com',
-                                #'Accept': 'application/json, text/plain, */*',
-                                #'Content-Type': 'application/json;charset=UTF-8',
-                                'Accept': "*/*",
-                                'Accept-Encoding' : 'gzip, deflate, sdch, br',
-                                'Accept-Language': 'en-US,en;q=0.8',
-                                'Cache-Control' : 'max-age=0',
-                                'Connection': 'keep-alive',
-                                #'Upgrade-Insecure-Requests':1
-                             }
+            'user-agent': USER_AGENT_STR,
+            'Host': 'kite.zerodha.com',
+            # 'Accept': 'application/json, text/plain, */*',
+            # 'Content-Type': 'application/json;charset=UTF-8',
+            'Accept': "*/*",
+            'Accept-Encoding': 'gzip, deflate, sdch, br',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
+            # 'Upgrade-Insecure-Requests':1
+        }
         super(KiteFront, self).__init__(*args, **kwargs)
 
     def connect(self):
@@ -74,19 +75,17 @@ class KiteFront(KiteConnect):
         params = self._qna(self.resp.text)
         return self._login_step2(params)
 
-
     def _login_step1(self):
         params = dict(user_id=self._auth['user_id'], password=self._auth['password'], login='')
 
         self.resp = self.session.post(LOGIN_SUBMIT_URL,
-                                      proxies = self.proxies,
-                                      data = params,
-                                      headers = self.headers_normal,
-                                      verify = False)
-        return self.resp.text.find('Security') >=0
+                                      proxies=self.proxies,
+                                      data=params,
+                                      headers=self.headers_normal,
+                                      verify=False)
+        return self.resp.text.find('Security') >= 0
 
-
-    def _qna(self,txt):
+    def _qna(self, txt):
         params = dict()
         self.soup = BeautifulSoup(txt, 'html.parser')
         form = self.soup.find_all('form')[0]
@@ -102,25 +101,25 @@ class KiteFront(KiteConnect):
 
     def _login_step2(self, params):
         self.resp = self.session.post(LOGIN_SUBMIT_URL,
-                                      proxies = self.proxies,
-                                      data = params,
-                                      headers = self.headers_normal,
-                                      verify = False)
+                                      proxies=self.proxies,
+                                      data=params,
+                                      headers=self.headers_normal,
+                                      verify=False)
         self.soup = BeautifulSoup(self.resp.text, 'html.parser')
-
 
         return self._check_step2()
 
     def _check_step2(self):
-        return (self.resp.text.find('Positions')) >= 0   
+        return (self.resp.text.find('Positions')) >= 0
 
-    # orders
+        # orders
+
     def order_place(self,
                     exchange,
                     tradingsymbol,
-                    transaction_type,
+                    transaction_type, #BUY, SELL
                     quantity,
-                    order_type,
+                    order_type, #MARKET, LIMIT, SL, SLM
                     price=0,
                     product="CNC",
                     validity="DAY",
@@ -133,8 +132,11 @@ class KiteFront(KiteConnect):
                     ):
         """Place an order."""
         params = locals()
-        del(params["self"])
+        del (params["self"])
         return self._post("orders.place", params)["order_id"]
+    
+
+    
 
     def _request(self, route, method, parameters=None):
         """Make an HTTP request."""
@@ -165,7 +167,6 @@ class KiteFront(KiteConnect):
         else:
             url = self._root + uri
 
-
         try:
             if self.debug:
                 print(method)
@@ -173,17 +174,18 @@ class KiteFront(KiteConnect):
                 print(params if method == "POST" else None)
                 print(params if method != "POST" else None)
             r = self.session.request(method,
-                    url,
-                    json=params if method == "POST" else None,
-                    params=params if method != "POST" else None,
-                    headers=self.headers_json,
-                    verify=False,
-                    allow_redirects=True,
-                    timeout=self._timeout,
-                    proxies=self.proxies)
-            self.r = r
+                                     url,
+                                     json=params if method in ["PUT", "POST"] else None,
+                                     params=params if method != "POST" else None,
+                                     headers=self.headers_json,
+                                     verify=False,
+                                     allow_redirects=True,
+                                     timeout=self._timeout,
+                                     proxies=self.proxies)
+            self.resp = r
             if self.debug:
-                import pdb; pdb.set_trace()
+                import pdb;
+                pdb.set_trace()
         except requests.ConnectionError:
             raise ex.ClientNetworkException("Gateway connection error", code=503)
         except requests.Timeout:
@@ -192,7 +194,6 @@ class KiteFront(KiteConnect):
             raise ex.ClientNetworkException("Invalid response from gateway", code=502)
         except Exception as e:
             raise ex.ClientNetworkException(e.message, code=500)
-
 
         # Validate the content type.
         if "json" in r.headers["content-type"]:
@@ -211,17 +212,17 @@ class KiteFront(KiteConnect):
                 # native Kite errors
                 exp = getattr(ex, data["error_type"])
                 if data["error_type"] == "TwoFAException":
-                    raise(ex.TwoFAException(data["message"],
-                                            questions=data["questions"] if "questions" in data else [],
-                                            code=r.status_code))
+                    raise (ex.TwoFAException(data["message"],
+                                             questions=data["questions"] if "questions" in data else [],
+                                             code=r.status_code))
                 elif exp:
-                    raise(exp(data["message"], code=r.status_code))
+                    raise (exp(data["message"], code=r.status_code))
                 else:
-                    raise(ex.GeneralException(data["message"], code=r.status_code))
+                    raise (ex.GeneralException(data["message"], code=r.status_code))
 
             return data["data"]
         elif "csv" in r.headers["content-type"]:
             return r.content
         else:
-            raise ex.DataException("Unknown Content-Type (%s) in response: (%s)" % (r.headers["content-type"], r.content))
-
+            raise ex.DataException(
+                "Unknown Content-Type (%s) in response: (%s)" % (r.headers["content-type"], r.content))
